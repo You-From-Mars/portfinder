f4385619293b4553ea2c85bc3b181d2cffbbcf74
TingYinHelen <yinhelen.hlj@qq.com>
2019-10-16 15:44:24 +0800

    修改测试代码

--- test/getPort.js
+++ test/getPort.js
@@ -1,26 +1,34 @@
 const Benchmark = require('benchmark')
-const suit = new Benchmark.Suite
+const suite = new Benchmark.Suite
 
 const serverPortfinder = require('../lib/index.server.js')
 const connectPortfinder = require('../lib/index.connect.js')
 
-const serverFinter = serverPortfinder.getPort((err, port) => {
-    if (err) {
-        console.error('listen error:', err)
-    } else {
-        console.log('listen port:', port)    
-    }
-})
+function serverFinter() {
+    serverPortfinder.getPort((err, port) => {
+        if (err) {
+            console.error('listen error:', err)
+        } else {
+            console.log('listen port:', port)    
+        }
+    })
+}
 
-const connectFinder = connectPortfinder.getPort((err, port) => {
-    if (err) {
-        console.error('connect error:', err)
-    } else {
-        console.log('connect port', port)
-    }
-})
+function connectFinder() {
+    connectPortfinder.getPort((err, port) => {
+        if (err) {
+            console.error('connect error:', err)
+        } else {
+            console.log('connect port', port)
+        }
+    })
+}
 
-suit
+// serverFinter()
+// connectFinder()
+
+
+suite
 .add('listen', serverFinter)
 .add('connect', connectFinder)
 .on('cycle', function (event) {
@@ -29,4 +37,4 @@ suit
 .on('complete', function () {
     console.log('Fastest is ' + this.filter('fastest').map('name'));
 })
-.run({ 'async': true });
+.run({ 'async': true })
