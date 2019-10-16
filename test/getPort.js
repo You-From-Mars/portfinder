583e01079ab1c42841f44f372a89499d4cb22c53
TingYinHelen <yinhelen.hlj@qq.com>
2019-10-16 14:02:04 +0800

    feat: 添加connect

--- test/getPort.js
+++ test/getPort.js
@@ -1,9 +1,32 @@
-const portfinder = require('../lib/index.js')
+const Benchmark = require('benchmark')
+const suit = new Benchmark.Suite
 
-portfinder.getPort((err, port) => {
+const serverPortfinder = require('../lib/index.server.js')
+const connectPortfinder = require('../lib/index.connect.js')
+
+const serverFinter = serverPortfinder.getPort((err, port) => {
     if (err) {
-        console.error(err)
+        console.error('listen error:', err)
     } else {
-        console.log('port:', port)    
+        console.log('listen port:', port)    
     }
 })
+
+const connectFinder = connectPortfinder.getPort((err, port) => {
+    if (err) {
+        console.error('connect error:', err)
+    } else {
+        console.log('connect port', port)
+    }
+})
+
+suit
+.add('listen', serverFinter)
+.add('connect', connectFinder)
+.on('cycle', function (event) {
+    console.log(String(event.target));
+})
+.on('complete', function () {
+    console.log('Fastest is ' + this.filter('fastest').map('name'));
+})
+.run({ 'async': true });
